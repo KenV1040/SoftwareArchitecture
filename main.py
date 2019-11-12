b53f8153d5f6d0dc2d6fd32657a07266656e6a85
Ken <ken.vu@probablyrational.com>
2019-11-12 20:40:38 +0000

    Changed main.py again

--- main.py
+++ main.py
@@ -1,39 +1,3 @@
-# #imports
-# import requests
-# import spotipy
-# from spotipy.oauth2 import SpotifyClientCredentials
-
-# def analyseEmotion(headline):
-# 	print("yo")
-
-# def newsAPI():
-# 	url = "https://newsapi.org/v2/top-headlines?country=gb"
-# 	response = requests.get(
-# 	url,
-# 	headers={
-# 		"Authorization":"b31dbb029fbc45c693c90dfad3065ee0"
-# 	}
-# 	)
-# 	res = response.json()
-# 	print("Total results: ", res['totalResults'])
-# 	for articles in res['articles']:
-# 		print(articles)
-
-# def spotifyAuth():
-# 	clientID = "75c5a1900edd49d19af217ca24cb6c97"
-# 	secret = "8d9eebd3551b4df68d8f1cdb3ea7b970" 
-# 	client_credentials_manager = SpotifyClientCredentials(
-# 	client_id=clientID, client_secret=secret
-# 	)
-# 	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-# def main():
-# 	newsAPI()
-
-# if __name__ == "__main__":
-# 	main()
-
-
 import json
 from flask import Flask, request, redirect, g, render_template
 import requests
@@ -41,6 +5,19 @@ from urllib.parse import quote
 
 # Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
 # Visit this url to see all the steps, parameters, and expected response.
+""" 
+# Get profile data
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
+
+    # Get user playlist data
+    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
+    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
+    playlist_data = json.loads(playlists_response.text)
+
+    # Combine profile and playlist data to display
+    display_arr = [profile_data] + playlist_data["items"] """
 
 
 app = Flask(__name__)
@@ -102,25 +79,24 @@ def callback():
     refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
-
+    newsResponse = getNewsAPI()
     # Auth Step 6: Use the access token to access Spotify API
     authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+    
+    return render_template("index.html", sorted_array=newsResponse['articles'])
 
-    # Get profile data
-    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
-    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
-    profile_data = json.loads(profile_response.text)
 
-    # Get user playlist data
-    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
-    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
-    playlist_data = json.loads(playlists_response.text)
-
-    # Combine profile and playlist data to display
-    display_arr = [profile_data] + playlist_data["items"]
-    return render_template("index.html", sorted_array=display_arr)
-
+def getNewsAPI():
+    apiKey = "b31dbb029fbc45c693c90dfad3065ee0"
+    country = "gb"
+    url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={apiKey}"
+    print("Getting news api from " + url)
+    response = requests.get(
+        url
+    )
+    newsResponse = json.loads(response.text)
+    print(newsResponse)
+    return newsResponse
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
-
